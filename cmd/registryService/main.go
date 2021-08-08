1fdd27545d813d82bdddb0469c54ebf2f8062efe
Farmer.Chillax <farmer-chong@qq.com>
2021-08-08 21:29:36 +0800

    feat: 构建server、registry的基本功能

---
+++ cmd/registryService/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"balance/registry"
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.Handle("/services", &registry.RegistryService{})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	var srv http.Server
+	srv.Addr = registry.ServerPort
+
+	go func() {
+		log.Println(srv.ListenAndServe())
+		cancel()
+	}()
+
+	go func() {
+		fmt.Printf("Service is running in %v\n", registry.ServicesURL)
+		fmt.Println("Registry service started. Press any key to stop.")
+		var s string
+		fmt.Scanln(&s)
+		srv.Shutdown(ctx)
+		cancel()
+	}()
+
+	<-ctx.Done()
+	fmt.Println("Shutting down registry service.")
+}
