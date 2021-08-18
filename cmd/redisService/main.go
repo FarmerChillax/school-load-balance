79cfb72208a3d3fe52f88a554779274c5d2bd5b2
Farmer.Chillax <farmer-chong@qq.com>
2021-08-19 01:35:43 +0800

    feat: add config from command args

--- cmd/redisService/main.go
+++ cmd/redisService/main.go
@@ -6,12 +6,64 @@ import (
 	"balance/service"
 	"balance/storage"
 	"context"
+	"encoding/json"
 	"fmt"
 	stlog "log"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
+type config struct {
+	Host     string `json:"host"`
+	Port     int    `json:"port"`
+	Protocol string `json:"protocol"`
+}
+
+func (c *config) LoadConfig(path string) error {
+	args := strings.Split(path, ".")
+	if args[len(args)-1] != "json" {
+		return fmt.Errorf("文件扩展名错误")
+	}
+	file, err := os.OpenFile(path, os.O_RDONLY, 0600)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(c)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func initConfig() (c config, err error) {
+	path := os.Args[len(os.Args)-1]
+	err = c.LoadConfig(path)
+	if err != nil {
+		return c, err
+	}
+	return c, nil
+}
+
 func main() {
-	host, port := "localhost", "7001"
+	ex, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+	exPath := filepath.Dir(ex)
+	fmt.Println(exPath)
+
+	// host, port := "localhost", "7001"
+	config, err := initConfig()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	host, port := config.Host, fmt.Sprintf("%d", config.Port)
+
 	serviceAddress := fmt.Sprintf("http://%s:%s", host, port)
 
 	r := registry.Registration{
